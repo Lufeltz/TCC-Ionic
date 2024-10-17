62b2c6bbed528d33a3d1a1fb802c7a9ab9d124e9
z4sk <lufelt@proton.me>
2024-10-17 11:01:46 -0300

    Adicioar estilo modalidades e conquistas

--- src/app/pages/modalidades/modalidades.page.ts
+++ src/app/pages/modalidades/modalidades.page.ts
@@ -15,6 +15,14 @@ import {
 } from '@ionic/angular/standalone';
 import { MenuPerfilComponent } from 'src/app/components/menu-perfil/menu-perfil.component';
 
+interface Modalidade {
+  nome: string;
+  descricao: string;
+  foto: string | null;
+  dataCriacao: string;
+  inscrito: boolean;
+}
+
 @Component({
   selector: 'app-modalidades',
   templateUrl: './modalidades.page.html',
@@ -42,6 +50,8 @@ export class ModalidadesPage implements OnInit {
   pageContent: string = 'modalidades';
 
   selectedSegment: string = 'inscrito';
+  modalidades: Modalidade[] = [];
+  currentModalidade: Modalidade | null = null; // Para armazenar a modalidade atual
 
   constructor() {}
 
@@ -57,7 +67,16 @@ export class ModalidadesPage implements OnInit {
       text: 'Confirmar',
       role: 'confirmar',
       handler: () => {
-        console.log('Alert confirmed');
+        if (this.currentModalidade) {
+          // Aqui você altera a lógica dependendo do estado atual
+          this.currentModalidade.inscrito = !this.currentModalidade.inscrito;
+          const action = this.currentModalidade.inscrito
+            ? 'inscrito'
+            : 'cancelado';
+          console.log(
+            `Modalidade ${this.currentModalidade.nome} agora está ${action}: ${this.currentModalidade.inscrito}`
+          );
+        }
       },
     },
   ];
@@ -66,5 +85,52 @@ export class ModalidadesPage implements OnInit {
     console.log(`Dismissed with role: ${ev.detail.role}`);
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.modalidades = [
+      {
+        nome: 'Futebol',
+        descricao:
+          'Esporte jogado por dois times, onde o objetivo é marcar gols movendo a bola com os pés. Vence quem marcar mais gols em dois tempos de partida.',
+        foto: null,
+        dataCriacao: '2024-10-09T17:38:29Z',
+        inscrito: true,
+      },
+      {
+        nome: 'Vôlei',
+        descricao:
+          'Dois times tentam fazer a bola tocar o chão do lado adversário, passando-a por cima de uma rede. Vence quem ganhar 3 sets.',
+        foto: null,
+        dataCriacao: '2024-10-09T17:38:29Z',
+        inscrito: false,
+      },
+      {
+        nome: 'Basquete',
+        descricao:
+          'Dois times tentam marcar pontos arremessando a bola na cesta do adversário. Ganha quem fizer mais pontos em quatro períodos.',
+        foto: null,
+        dataCriacao: '2024-10-09T17:38:29Z',
+        inscrito: true,
+      },
+      {
+        nome: 'Tênis de Mesa',
+        descricao:
+          'Jogadores usam raquetes para golpear uma bola em uma mesa com rede. O objetivo é fazer o adversário errar a devolução. Vence quem ganhar mais sets.',
+        foto: null,
+        dataCriacao: '2024-10-09T17:38:29Z',
+        inscrito: false,
+      },
+    ];
+  }
+
+  getModalidadesInscritas() {
+    return this.modalidades.filter((modalidade) => modalidade.inscrito);
+  }
+
+  getModalidadesNaoInscritas() {
+    return this.modalidades.filter((modalidade) => !modalidade.inscrito);
+  }
+
+  setCurrentModalidade(modalidade: Modalidade) {
+    this.currentModalidade = modalidade;
+  }
 }
